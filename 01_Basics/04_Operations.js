c7a49eb94d8e413403b5bef743c061f00fe9f00e
Aryan Srivastava <114677161+AryanSrivastava15@users.noreply.github.com>
2025-03-17 06:32:29 +0000

    Operations Done

---
+++ 01_Basics/04_Operations.js
@@ -0,0 +1,25 @@
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "Hello"
+let str2 = " Aryan"
+let str3= str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log("1" + "2")
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+console.log(+true) //returns 1
+console.log(+"") // return 0
+
+let gameCounter = 100
+gameCounter++
+
+console.log(gameCounter)
+
+++gameCounter;
+console.log(gameCounter)
