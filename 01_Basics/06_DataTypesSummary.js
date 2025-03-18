9e37b68ca4b92d36bf24379b61463f90421254b3
Aryan Srivastava <114677161+AryanSrivastava15@users.noreply.github.com>
2025-03-18 05:50:45 +0000

    Data Types Summary

---
+++ 01_Basics/06_DataTypesSummary.js
@@ -0,0 +1,32 @@
+// Primitivr DataTypes (If a copy is made and changes is made to copy it happens only to the copy not the original one)
+/*
+1. String (Type: String)
+2. Number (Type: Number)
+3. Boolean (Type: Boolean)
+4. Null (Type: Object)
+5. Undefined (Type: undefined)
+6. Symbol (Type: Symbol)
+7. BigInt (add n in end of number to declare bigInt)
+*/
+
+// Non-Primitive (Reference)
+/*
+1. Arrays (Type: Object)
+Example: ["name1", "name2", ......]
+2. Objects (Type: Object) 
+Example: 
+let myObj = {
+    name: "Aryan"
+    age: 20
+}
+3. Functions (Type: Object Function)
+Example:
+const myFunction = function(){
+    console.log("Hello World")
+}
+*/
+
+//Symbol declaration
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+// console.log(id == anotherId)  ==> false
