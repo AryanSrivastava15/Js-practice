1e2f15510a7d8401e39b60f9a166d03d397beccd
Aryan Srivastava <114677161+AryanSrivastava15@users.noreply.github.com>
2025-03-20 15:48:42 +0000

    Objects Part 3 Done

---
+++ 02_Basics - Intermediate/05_Objects(Part3).js
@@ -0,0 +1,29 @@
+const course = {
+    courseName: "Js in Hindi",
+    Price: "999",
+    courseInstructor: "Hitesh"
+}
+
+const {courseInstructor: instructor} = course
+
+console.log(instructor)
+
+// const navbar = ({company}) => {
+
+// }
+
+// navbar(company = "Aryan")  // The curly braces are used for destructuring
+
+// {
+//     "name": "Aryan",
+//     "courseName": "Js in Hindi",
+//     "price": "Free"
+// } // API JSON Format
+
+/*
+[
+    {},
+    {},
+    {}
+] // Array with objects in API's
+*/
