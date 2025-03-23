66472656d6ead9bdf64383283732e6062704c18e
Aryan Srivastava <114677161+AryanSrivastava15@users.noreply.github.com>
2025-03-23 13:57:39 +0000

    While Loops Done

---
+++ 05_Iterations/02_While.js
@@ -0,0 +1,15 @@
+// While Loops
+/*
+Syntax:
+while (condition) {
+    
+}
+*/
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`)
+    index++
+}
+
+// Similarly can do for Arrays
