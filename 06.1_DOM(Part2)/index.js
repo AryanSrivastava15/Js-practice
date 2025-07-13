525fd389913f546007ea7371bf4642724a02c597
Aryan Srivastava <114677161+AryanSrivastava15@users.noreply.github.com>
2025-07-13 23:48:39 +0530

    DOM Completed

--- 06.1_DOM(Part2)/index.js
+++ 06.1_DOM(Part2)/index.js
@@ -1,3 +1,6 @@
+// Traversal in JavaScript
+
+
 // const parent = document.querySelector('.parent')
 // // console.log(parent);
 // // console.log(parent.children[1].innerHTML);
@@ -18,15 +21,57 @@
 // console.log("NODES: ", parent.childNodes)
 // //Every line break and comments and code are part of the tree structure in a NodeList.
 
-const div = document.createElement('div')
-console.log(div);
-div.className = "main"
-div.id = Math.round(Math.random() * 10 + 1)
-div.setAttribute("title", "generated title")
-div.style.backgroundColor = 'green'
-div.style.padding = "12px"
-//div.innerText = "Aryan"  // 1st way to add text
-const text = document.createTextNode("Aryan")  // 2nd way to add text
-div.appendChild(text)
 
-document.body.appendChild(div)
+// Adding Elements in DOM
+
+
+// const div = document.createElement('div')
+// console.log(div);
+// div.className = "main"
+// div.id = Math.round(Math.random() * 10 + 1)
+// div.setAttribute("title", "generated title")
+// div.style.backgroundColor = 'green'
+// div.style.padding = "12px"
+// //div.innerText = "Aryan"  // 1st way to add text
+// const text = document.createTextNode("Aryan")  // 2nd way to add text
+// div.appendChild(text)
+
+// document.body.appendChild(div)
+
+function addLanguage(langName){
+    const li = document.createElement('li');
+    li.innerHTML = `${langName}`;
+    document.querySelector('.language').appendChild(li)
+}
+
+addLanguage("Java")
+
+function addOptimised(langName){
+    const li = document.querySelector('li');
+    li.appendChild(document.createTextNode(langName))
+    document.querySelector('.language').appendChild(li)
+}
+// Using .createTextNode and than .appendChild is more optimised as we dont have to traverse complete tree.
+addLanguage("GoLang")
+
+// Editing in DOM
+
+const secondLang = document.querySelector("li:nth-child(2)")
+// secondLang.innerHTML = "Mojo"
+const newli = document.createElement('li')
+newli.textContent = "Mojo"
+secondLang.replaceWith(newli)
+
+const firstLang = document.querySelector("li:first-child")
+console.log(firstLang)
+firstLang.outerHTML = '<li>TyperScript</li>'
+
+// Removing in DOM
+
+const lastLang = document.querySelector("li:last-child");
+lastLang.remove()
+
+function remove(LangNumber){
+    const position = document.querySelector("li:nth-child(`${LangNumber}`)");
+    position.remove();
+} // Not correctly working
